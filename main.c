4f58922e4c50977458dfe09c781f9da060761c65
luisotavioliveiraDev <luisotavioliveira07dev@gmail.com>
2026-09-12 11:45:26 -0300

    Primeiro código em C

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/*Primeiro codigo em C */
+int main() {
+    printf("Ola, mundo!\n");
+    return 0;
+}
---
+++ soma.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*Programa para soma de dois numeros */
+int main() {
+    int a, b, soma; /*declaracao de variavel*/
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &a);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &b);
+    soma = a + b;
+printf("A soma de %d e %d e: %d\n", a, b, soma);
+    return 0;
+}
+
+
